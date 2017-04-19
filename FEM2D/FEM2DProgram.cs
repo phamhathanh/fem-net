7bd0806048334544d860a88b726e34e3cbc0d0ab
ThanhPH <phamhathanh130394@gmail.com>
2017-04-19 15:49:52 +0700

    Argument from command.

--- FEM2D/FEM2DProgram.cs
+++ FEM2D/FEM2DProgram.cs
@@ -1,34 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using static System.Math;
 
 namespace FEM_NET.FEM2D
 {
     internal static class FEM2DProgram
     {
-        private static IMesh mesh;
-        private static Dictionary<int, Func<Vector2, double>> boundaryConditions;
-        private const string PROBLEM_NAME = "heat2_cs";
-
-        public static void Run()
+        public static void Run(string meshName, string conditionFileName, double dt)
         {
-            Console.WriteLine("FEM for solving equation: du/dt - Laplace(u) + a0 * u = F");
+            Console.WriteLine("Solving...");
             var totalTimer = StartMeasuringTaskTime("Total");
 
             var readInputTimer = StartMeasuringTaskTime("Read input files");
-            bool success = TryReadInputFiles();
-            if (!success)
-                return;
+            var mesh = InOut.ReadMesh($"{meshName}.mesh", new P1Element.Factory());
+            var boundaryConditions = InOut.ReadBoundaryConditions($"{conditionFileName}");
 
             ShowMeshParameters(mesh);
             StopAndShowTaskTime(readInputTimer);
 
             var calculationTimer = StartMeasuringTaskTime("Calculation");
 
-            // TODO: Time step from file / command.
-            double a0 = 0,
-                dt = 0.1;
+            double a0 = 0;
             BilinearForm bilinearForm = (u, v, du, dv) => dt * Vector2.Dot(du, dv) + (1 + dt * a0) * u * v;
             Func<Vector2, double> f = v => 0,
                 u0 = v => 10 + 15*v.x;
@@ -41,30 +32,10 @@ namespace FEM_NET.FEM2D
                 var laplaceEquation = new Problem(mesh, boundaryConditions, bilinearForm, rhs);
                 previous = laplaceEquation.Solve();
             }
-            InOut.WriteSolutionToFile($"example{Path.DirectorySeparatorChar}{PROBLEM_NAME}.sol", mesh, previous);
+            InOut.WriteSolutionToFile($"{meshName}.sol", mesh, previous);
 
             StopAndShowTaskTime(calculationTimer);
             StopAndShowTaskTime(totalTimer);
-        }
-
-        private static bool TryReadInputFiles()
-        {
-            try
-            {
-                mesh = InOut.ReadMesh($"example{Path.DirectorySeparatorChar}{PROBLEM_NAME}.mesh", new P1Element.Factory());
-                boundaryConditions = InOut.ReadBoundaryConditions($"example{Path.DirectorySeparatorChar}DEFAULT.heat");
-            }
-            catch (FileNotFoundException exception)
-            {
-                Console.WriteLine($"FILE NOT FOUND: {exception.Message}");
-                return false;
-            }
-            catch (DirectoryNotFoundException exception)
-            {
-                Console.WriteLine($"DIRECTORY NOT FOUND: {exception.Message}");
-                return false;
-            }
-            return true;
         }
 
         private static Timer StartMeasuringTaskTime(string taskName)
--- Program.cs
+++ Program.cs
@@ -1,14 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FEM_NET
 {
     internal static class Program
     {
+        private static double dt = 0.1;
+        private static string meshName = null,
+            conditionFileName = "example\\DEFAULT.heat";
+
         private static void Main(string[] args)
         {
-            FEM2D.FEM2DProgram.Run();
+            Console.WriteLine();
+            try
+            {
+                ParseArgs(args);
+                FEM2D.FEM2DProgram.Run(meshName, conditionFileName, dt);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (FileNotFoundException exception)
+            {
+                Console.WriteLine($"FILE NOT FOUND: {exception.Message}");
+            }
+            catch (DirectoryNotFoundException exception)
+            {
+                Console.WriteLine($"DIRECTORY NOT FOUND: {exception.Message}");
+            }
+
             Console.WriteLine("Press ENTER to exit...");
             Console.ReadLine();
+        }
+
+        private static void ParseArgs(string[] args)
+        {
+            var argsQueue = new Queue<string>(args);
+            while (argsQueue.Count != 0)
+            {
+                var arg = argsQueue.Dequeue();
+                if (arg[0] != '-')
+                {
+                    if (meshName != null)
+                        Console.WriteLine($"Argument \"{arg}\" is ignored.");
+                    else
+                        meshName = arg;
+                        // TODO: Sanitize.
+                    continue;
+                }
+                switch (arg)
+                {
+                    case "-dt":
+                        if (argsQueue.Count == 0)
+                            throw new ArgumentException("ARGUMENT ERROR: Missing parameter for option \"-dt\".");
+                        dt = double.Parse(argsQueue.Dequeue());
+                        // TODO: Format exception.
+                    break;
+                    case "-bc":
+                        if (argsQueue.Count == 0)
+                            throw new ArgumentException("ARGUMENT ERROR: Missing parameter for option \"-bc\".");
+                        conditionFileName = argsQueue.Dequeue();
+                        // TODO: Sanitize.
+                    break;
+                    // TODO: -h / --help
+                    default:
+                        throw new ArgumentException($"ARGUMENT ERROR: Unknown option \"{arg}\".");
+                }
+            }
+            if (meshName == null)
+                throw new ArgumentException("ARGUMENT ERROR: Missing mesh name.");
+            if (meshName.EndsWith(".mesh"))
+                meshName = meshName.Substring(0, meshName.Length - 5);
         }
     }
 }
